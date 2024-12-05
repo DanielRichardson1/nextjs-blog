b6d7de2b0e8a95f765b6e5288944a6ca20a6d92e
danielrichardson1 <drich1475@gmail.com>
2024-12-05 00:00:52 -0500

    Add bottom section

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,27 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hello, I'm Daniel Richardson. I'm a computer engineer and a student at the University of Pittsburgh. You can contact me on{' '}
+          Hello, I'm <b>Daniel</b>. I'm a computer engineer and a student at the University of Pittsburgh. You can contact me on{' '}
           <a href="https://github.com/DanielRichardson1">Github</a>.
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
